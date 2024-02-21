201c9fcda18c9946f48220be599a10d1f99eb227
ahmed <ahmedmansoor3366@gmail.com>
2024-02-21 19:24:25 +0500

    Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
+/*
+ API to get breeds = https://dogapi.dog/api/v2/breeds
+
+ Have to get the list of dog breeds and do the following
+ 1. Map the data so that we get only the breed name and id
+ 2. List the data
+ 3. Add to the list
+ 4. Remove from the list
+ 5. Update the list
+*/
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="main-wrapper">
+                <div className={'header'}>
+                    Addition deletion and updates
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,25 @@
+dogBreeds = [{
+    "id": "68f47c5a-5115-47cd-9849-e45d3c378f12",
+    "name": "Caucasian Shepherd Dog"
+}, {
+    "id": "4ddbe251-72af-495e-8e9d-869217e1d92a",
+    "name": "Bouvier des Flandres"
+}, {
+    "id": "f534c847-bed1-4b58-b194-dc06ecfe20f9",
+    "name": "Grand Basset Griffon Vendéen"
+}, {"id": "30f62219-e225-42cd-bd07-02425f944c07", "name": "Hokkaido"}, {
+    "id": "087979f3-1c45-4d8a-a153-462bf5ea379e",
+    "name": "Japanese Terrier"
+}, {
+    "id": "dbff689b-8370-4b6a-9306-215aba549102",
+    "name": "Hanoverian Scenthound"
+}, {
+    "id": "6f540c30-27a8-48cc-8d88-0b1d9fa99167",
+    "name": "Tibetan Spaniel"
+}, {
+    "id": "20b1d8be-ae44-4a70-8526-0612904bc9b2",
+    "name": "Border Collie"
+}, {
+    "id": "6dee41b1-0805-4f4e-a079-c8b1cdfa1768",
+    "name": "Curly-Coated Retriever"
+}, {"id": "beff84c3-66c4-4335-beba-f346c2565881", "name": "Skye Terrier"}]
